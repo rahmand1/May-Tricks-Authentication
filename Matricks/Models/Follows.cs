7be1e305db628dfe6aac343f5ec8801498c3ff01
lucasstockton <35933381+lucasstockton@users.noreply.github.com>
2018-05-03 15:41:29 -0400

    Bug fixes for display problems and added database models for Follows and Messaging. Routing is someitmes working sometimes not.

---
+++ Matricks/Models/Follows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matricks.Models
+{
+    public class Follows
+    {
+
+        public int ID { get; set; }
+       
+        public int followingID { get; set; }
+        public int followerID { get; set; }
+    }
+}
---
+++ Matricks/Models/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matricks.Models
+{
+    public class Messages
+    {
+        public int ID { get; set; }
+        public int messagetoID { get; set; }
+        public int messagefromID { get; set; }
+        public string Message { get; set; }
+    }
+}
